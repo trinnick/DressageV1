793b648aadd0e5846b0cee2297511f3dc0733911
Douglas A. Stewart <douglas.alan.stewart@gmail.com>
2012-10-28 22:26:31 -0500

    Dressage11
    -Fixed Straight-Line motion algorithms to work with all XML parsed data
    -Still need to implement arced motion paths

--- Assets/Movement/HorseMovement.cs
+++ Assets/Movement/HorseMovement.cs
@@ -6,6 +6,7 @@ using System.Collections;
 public class HorseMovement : MonoBehaviour {
 	
 	public Vector3 defualt = new Vector3();
+	public float rateOfMovement = 0.3f;
 	
 	// Use this for initialization
 	IEnumerator Start () {
@@ -66,6 +67,7 @@ public class HorseMovement : MonoBehaviour {
 		Vector3 firstPosition = new Vector3();
 		Vector3 secondPosition = new Vector3();
 		Vector3 thirdPosition = new Vector3();
+		Vector3 fourthPosition = new Vector3();
 		
 		Debug.Log("Starting Position: " + transform.position);
 		
@@ -79,35 +81,76 @@ public class HorseMovement : MonoBehaviour {
 		secondPosition = GameObject.Find(path[1]).transform.position;
 		
 		//If there is a third element in path, assign it to the thirdPosition variable
-		if(2 < path.Length){
+		if(path.Length >= 3){
 			thirdPosition = GameObject.Find(path[2]).transform.position;
+		}
+		
+		//If there is a third element in path, assign it to the thirdPosition variable
+		if(path.Length >= 4){
+			fourthPosition = GameObject.Find(path[3]).transform.position;
 		}
 		
 		//Show movement based on path extraction in debugger
 		Debug.Log("Movement: " + movement.Path);
 		
-		//If there are more than 2 waypoints output that information and their positions to the debugger
-		if(3 == path.Length){
-			Debug.Log(path[0] + ": " + firstPosition + "\n" + path[1] + ": " + secondPosition + "\n" + path[2] + ": " + thirdPosition);
-		}
-		//If there are only 2 waypoints output that information to the debugger log
-		else{
+		switch(path.Length){
+		case 2:
 			Debug.Log(path[0] + ": " + firstPosition + "\n" + path[1] + ": " + secondPosition);
+			break;
+		case 3:
+			Debug.Log(path[0] + ": " + firstPosition + "\n" + path[1] + ": " + secondPosition + "\n" + path[2] + ": " + thirdPosition);
+			break;
+		case 4:
+			Debug.Log(path[0] + ": " + firstPosition + "\n" + path[1] + ": " + secondPosition + "\n" + path[2] + ": " + thirdPosition + "\n" + path[3] + ": " + fourthPosition);
+			break;
+		default:
+			Debug.Log("Problem Processing path array");
+			break;
 		}
 		
 		//Call movement based on Coroutine and yield commands
 
 		yield return StartCoroutine(firstMovement(firstPosition,secondPosition));
+		
+		if(path.Length >= 3){
+			yield return StartCoroutine(secondMovement(secondPosition,thirdPosition));
+		}
+		
+		if(path.Length >= 4){
+			yield return StartCoroutine(thirdMovement(thirdPosition,fourthPosition));
+		}
 	}
 	
 	IEnumerator firstMovement(Vector3 first, Vector3 second){
 	    float i = 0.0f;
-	    float rate = 0.1f;
 		
 	    while (i < 1.0f) {
-			Debug.Log("Inside First Loop");
-	        i += Time.deltaTime * rate;
+	        i += Time.deltaTime * rateOfMovement;
 	        transform.position = Vector3.Lerp(first, second, i);
+			yield return null;
+	    }
+			
+		Debug.Log("Ending Position: " + transform.position);
+	}
+	
+	IEnumerator secondMovement(Vector3 second, Vector3 third){
+	    float i = 0.0f;
+		
+	    while (i < 1.0f) {
+	        i += Time.deltaTime * rateOfMovement;
+	        transform.position = Vector3.Lerp(second, third, i);
+			yield return null;
+	    }
+			
+		Debug.Log("Ending Position: " + transform.position);
+	}
+	
+	IEnumerator thirdMovement(Vector3 third, Vector3 fourth){
+	    float i = 0.0f;
+		
+	    while (i < 1.0f) {
+	        i += Time.deltaTime * rateOfMovement;
+	        transform.position = Vector3.Lerp(third, fourth, i);
 			yield return null;
 	    }
 			
